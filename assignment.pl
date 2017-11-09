ece0d39221f4453ba3c572c27c159ff2f844a899
Andre Godinez <a.godinez1@nuigalway.ie>
2017-11-09 15:46:11 +0000

    Add files via upload

--- assignment.pl
+++ assignment.pl
@@ -9,6 +9,28 @@ isElementInList(X,[_|T]):-
 
 %Definitions for reverseList(List, ReversedList)
 
+%base case
+reverseList([],[]).
+
+reverseList([H|T], ReversedList):-
+	%we get the Tail of list and reverse it
+    reverseList(T, ReversedT), 
+	%after all tail is reversed concatenate reversed Tail with head
+	%swi uses append/3 instead of conc/3
+	append(ReversedT, [H], ReversedList).
+
+
 %insertElementIntoListEnd(El, List, NewList)
+%base case insert element into empty list returns list
+%with single element
+%insertElementIntoListEnd(El,[],[El]).
+
+insertElementIntoListEnd(El,List,X):-
+	append(List,[El],X).
 
 %Definitions for mergeLists(List1, List2, Merged)
+%base case
+mergeLists([List1],[],[List1]).
+
+mergeLists(List1,List2,Merged):-
+		append(List1,List2,Merged).
